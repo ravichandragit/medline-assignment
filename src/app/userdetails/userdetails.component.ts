4becdb3efa042b3c856a8b46f4c0c1cc1f2c3085
ravichandragit <ravi.honamore@gmail.com>
2020-10-14 19:32:24 +0530

    form module added input validation done

--- src/app/userdetails/userdetails.component.ts
+++ src/app/userdetails/userdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-userdetails',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserdetailsComponent implements OnInit {
 
+  firstname:string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onuserSubmit(frm : NgForm){
+    console.log(frm.value);
   }
 
 }
